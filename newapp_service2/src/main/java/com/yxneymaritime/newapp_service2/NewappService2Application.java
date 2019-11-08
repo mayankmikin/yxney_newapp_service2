a9058f540fa261912cb76598a17ed521080a0dcc
Mayank Verma <mayank.mikin@gmail.com>
2019-11-08 10:04:36 +0530

    service 2

---
+++ newapp_service2/src/main/java/com/yxneymaritime/newapp_service2/NewappService2Application.java
@@ -0,0 +1,13 @@
+package com.yxneymaritime.newapp_service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewappService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewappService2Application.class, args);
+	}
+
+}
---
+++ newapp_service2/src/test/java/com/yxneymaritime/newapp_service2/NewappService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxneymaritime.newapp_service2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewappService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
